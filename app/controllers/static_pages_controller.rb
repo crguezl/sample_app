e5aec8441a8271f45855aaef469b836cf5e9c176
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-06-05 13:57:13 +0100

    added StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+  def home
+    @chuchu = "hello!"
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def help
+    @titi = "Ayudaaaa!!!!!!"
+    respond_to do |format|
+      format.html
+    end
+  end
+end
